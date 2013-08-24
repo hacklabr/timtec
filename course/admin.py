e34f5e1eb53c416d9ea91e9247cd10c45808df0f
Leonardo Santagada <santagada@gmail.com>
2013-08-24 00:18:52 -0300

    Inline sortable for lessons

--- course/admin.py
+++ course/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from suit.admin import SortableTabularInline
+
 from models import *
 
 admin.site.register(TimtecUser)
 admin.site.register(Video)
-admin.site.register(Course)
 admin.site.register(CourseProfessor)
+
+class LessonInline(SortableTabularInline):
+    model = Lesson
+    sortable = 'position'
+
+class CourseAdmin(ModelAdmin):
+    inlines = (LessonInline,)
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
 admin.site.register(Activity)
 admin.site.register(Unit)
