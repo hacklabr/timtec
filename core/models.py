24449866ead8dd8a33045e7fcfdb46e5bbfa86e3
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2013-08-26 14:52:02 -0300

    add labels for fields in models

--- core/models.py
+++ core/models.py
@@ -18,6 +18,11 @@ class Video(models.Model):
     name = models.CharField(max_length=255)
     youtube_id = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = _('Video')
+        verbose_name_plural = _('Videos')
+
+
     def __unicode__(self):
         return self.name
 
@@ -29,18 +34,22 @@ class Course(models.Model):
         ('public', _('Public')),
     )
 
-    slug = models.SlugField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    intro_video = models.ForeignKey(Video)
-    application = models.TextField()
-    requirement = models.TextField()
-    abstract = models.TextField()
-    structure = models.TextField()
-    workload = models.TextField()
-    pronatec = models.TextField()
-    status = models.CharField(choices=STATES, default=STATES[0][0], max_length=128)
-    publication = models.DateField()
+    slug = models.SlugField(_('Slug'), max_length=255, unique=True)
+    name = models.CharField(_('Name'), max_length=255)
+    intro_video = models.ForeignKey(Video, verbose_name=_('Intro video'))
+    application = models.TextField(_('Application'))
+    requirement = models.TextField(_('Requirement'))
+    abstract = models.TextField(_('Abstract'))
+    structure = models.TextField(_('Structure'))
+    workload = models.TextField(_('Workload'))
+    pronatec = models.TextField(_('Pronatec'))
+    status = models.CharField(_('Status'), choices=STATES, default=STATES[0][0], max_length=128)
+    publication = models.DateField(_('Publication'), )
     professors = models.ManyToManyField(TimtecUser, through='CourseProfessor')
+
+    class Meta:
+        verbose_name = _('Course')
+        verbose_name_plural = _('Courses')
 
     def __unicode__(self):
         return self.name
@@ -49,23 +58,27 @@ class Course(models.Model):
 class CourseProfessor(models.Model):
     class Meta:
         unique_together = (('user', 'course'),)
+        verbose_name = _('Course Professor')
+        verbose_name_plural = _('Course Professors')
 
     user = models.ForeignKey(TimtecUser, verbose_name=_('Professor'))
-    course = models.ForeignKey(Course)
-    biography = models.TextField()
+    course = models.ForeignKey(Course, verbose_name=_('Course'))
+    biography = models.TextField(_('Biography'))
 
     def __unicode__(self):
         return u'%s @ %s' % (self.user, self.course)
 
 
 class Lesson(models.Model):
-    slug = models.SlugField(max_length=255, editable=False, unique=True)
-    name = models.CharField(max_length=255)
-    desc = models.CharField(max_length=255)
-    position = models.PositiveIntegerField()
-    course = models.ForeignKey(Course)
+    slug = models.SlugField(_('Slug'), max_length=255, editable=False, unique=True)
+    name = models.CharField(_('Name'), max_length=255)
+    desc = models.CharField(_('Description'), max_length=255)
+    position = models.PositiveIntegerField(_('Position'))
+    course = models.ForeignKey(Course, verbose_name=_('Course'))
 
     class Meta:
+        verbose_name = _('Lesson')
+        verbose_name_plural = _('Lessons')
         ordering = ['position']
 
     def save(self, **kwargs):
@@ -90,11 +103,12 @@ class Activity(models.Model):
         data: {question: "", choices: ["choice1", "choice2", ...]}
         expected_answer_data: {choice: 1}
     """
-    type = models.CharField(max_length=255)
-    data = JSONField()
-    expected_answer = JSONField()
+    type = models.CharField(_('Type'), max_length=255)
+    data = JSONField(_('Data'))
+    expected_answer = JSONField(_('Expected answer'))
 
     class Meta:
+        verbose_name = _('Activity')
         verbose_name_plural = _('Activities')
 
     def __unicode__(self):
@@ -102,12 +116,14 @@ class Activity(models.Model):
 
 
 class Unit(models.Model):
-    lesson = models.ForeignKey(Lesson)
-    video = models.ForeignKey(Video, null=True, blank=True)
-    activity = models.ForeignKey(Activity, null=True, blank=True)
-    position = models.PositiveIntegerField()
+    lesson = models.ForeignKey(Lesson, verbose_name=_('Lesson'))
+    video = models.ForeignKey(Video, verbose_name=_('Video'), null=True, blank=True)
+    activity = models.ForeignKey(Activity, verbose_name=_('Activity'), null=True, blank=True)
+    position = models.PositiveIntegerField(_('Position'))
 
     class Meta:
+        verbose_name = _('Unit')
+        verbose_name_plural = _('Units')
         ordering = ['position']
 
     def __unicode__(self):
@@ -115,7 +131,11 @@ class Unit(models.Model):
 
 
 class Answer(models.Model):
-    activity = models.ForeignKey(Activity)
-    user = models.ForeignKey(TimtecUser)
+    activity = models.ForeignKey(Activity, verbose_name=_('Activity'))
+    user = models.ForeignKey(TimtecUser, verbose_name=_('Professor'))
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
-    answer = models.TextField()
+    answer = models.TextField(_('Answer'))
+
+    class Meta:
+        verbose_name = _('Answer')
+        verbose_name_plural = _('Answers')
