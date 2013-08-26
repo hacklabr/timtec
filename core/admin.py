9dbc4f46b10c26763f19ad0cb4341b272607285b
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2013-08-26 08:56:32 -0300

    ajusta a visualização de algumas páginas e listas no admin

--- core/admin.py
+++ core/admin.py
@@ -1,25 +1,52 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
+
+from django.forms import TextInput, Textarea
+from django.db import models
+
 from suit.admin import SortableTabularInline
 
 from models import *
 
-admin.site.register(TimtecUser, UserAdmin)
-admin.site.register(Video)
-admin.site.register(CourseProfessor)
+
+class ModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'class':'span12'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols': 0, 'class':'span12'})},
+    }
 
 
 class LessonInline(SortableTabularInline):
     model = Lesson
     sortable = 'position'
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows':3, 'class':'span11'})},
+    }
+
+
+class LessonAdmin(ModelAdmin):
+    list_display = ('name', 'course',)
 
 
 class CourseAdmin(ModelAdmin):
+    list_display = ('name', 'status', 'publication',)
     inlines = (LessonInline,)
 
+
+class CourseProfessorAdmin(ModelAdmin):
+    list_display = ('user', 'course',)
+
+
+class VideoAdmin(ModelAdmin):
+	list_display = ('name', 'youtube_id',)
+
+
+admin.site.register(TimtecUser, UserAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(CourseProfessor, CourseProfessorAdmin)
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Activity)
 admin.site.register(Unit)
 admin.site.register(Answer)
