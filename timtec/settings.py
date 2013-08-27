be49a7091695de3b0b11765924f42b71c4010380
Leonardo Santagada <santagada@hacklab.com.br>
2013-08-27 10:55:26 -0300

    default backend should be console

--- timtec/settings.py
+++ timtec/settings.py
@@ -13,6 +13,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
