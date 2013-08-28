1e9252ddc54605d55517fc3f89b79acd0709321c
Leonardo Santagada <santagada@hacklab.com.br>
2013-08-28 18:00:33 -0300

    moved a fixture to the conftest common place and used in a test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,9 +1,10 @@
 import pytest
 from core.models import Lesson
 
+
 @pytest.mark.django_db
-def test_lesson(client):
+def test_lesson(admin_client):
     lesson = Lesson.objects.all()[0]
-    response = client.get('/lesson/' + lesson.slug)
+    response = admin_client.get('/lesson/' + lesson.slug)
     assert response.status_code == 200
     assert lesson.name.encode('utf-8') in response.content
