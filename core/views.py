77f76e64abd54b43f85e07d2dbe33097bdfb3b93
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2013-09-03 17:18:38 -0300

    remove template tag solution

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.views.generic import DetailView
 from django.views.generic.base import RedirectView, View, TemplateView
 from accounts.utils import LoginRequiredMixin
 
-from models import Course
+from models import Course, StudentProgress
 
 
 class HomeView(View):
@@ -17,6 +17,19 @@ class HomeView(View):
 class CourseIntroView(DetailView):
     model = Course
     template_name = 'course-intro.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseIntroView, self).get_context_data(**kwargs)
+
+        units_done = []
+        if self.request.user.is_authenticated:
+            units_done = StudentProgress.objects.filter(user=self.request.user, unit__lesson__course=self.object)\
+                                                .exclude(complete=None)\
+                                                .values_list('unit', flat=True)
+        context['units_done'] = units_done
+        return context
+
 
 
 class UserCoursesView(LoginRequiredMixin,TemplateView):
