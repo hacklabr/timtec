b48cee1d6dde7f59874444c4d678db71dd88f458
Leonardo Santagada <santagada@hacklab.com.br>
2013-09-05 15:52:57 -0300

    set default from email on the staging server

--- timtec/settings_local_staging.py
+++ timtec/settings_local_staging.py
@@ -3,3 +3,4 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_SUBJECT_PREFIX = '[timtec-staging]'
+DEFAULT_FROM_EMAIL = 'timtec-staging@timtec.com.br'
