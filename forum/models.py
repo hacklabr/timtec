8a7b46fc3c0703dfeee158a71415d51d39e2fe36
Bruno Martin <bruno.martin@hacklab.com.br>
2013-09-13 15:02:14 -0300

    added slug field in question

--- forum/models.py
+++ forum/models.py
@@ -2,17 +2,24 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.template.defaultfilters import slugify
 from core.models import Course, Lesson
 
 
 class Question(models.Model):
     title = models.CharField(_('Title'), max_length=255)
     text = models.TextField(_('Question'))
+    slug = models.SlugField(_('Slug'), max_length=255, editable=False, unique=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'))
     correct_answer = models.OneToOneField('Answer', verbose_name=_('Correct answer'), related_name='+', null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
     course = models.ForeignKey(Course, verbose_name=_('Course'))
     lesson = models.ForeignKey(Lesson, verbose_name=_('Lesson'), null=True, blank=True)
+
+    def save(self, **kwargs):
+        if not self.id and self.title:
+            self.slug = slugify(self.title)
+        super(Question, self).save(**kwargs)
 
     def __unicode__(self):
         return self.title
