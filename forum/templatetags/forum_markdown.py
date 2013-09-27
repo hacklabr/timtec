4a46adfaa2051dd575de18d2072dc85e3540905b
Bruno Martin <bruno.martin@hacklab.com.br>
2013-09-27 18:45:33 -0300

    creates markdown template filter; add markdown to requirements

---
+++ forum/templatetags/forum_markdown.py
@@ -0,0 +1,19 @@
+import markdown
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def my_markdown(value):
+    extensions = ["nl2br", ]
+
+    return mark_safe(markdown.markdown(force_unicode(value),
+                                       extensions,
+                                       safe_mode=True,
+                                       enable_attributes=False))
