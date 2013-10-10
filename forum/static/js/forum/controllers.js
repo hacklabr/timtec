5fdb4521bc98101327b66dbb4c3c00fbf701f991
Bruno Martin <bruno.martin@hacklab.com.br>
2013-10-10 16:56:03 -0300

    hide editor to add answer if the current user already answerd the current question

--- forum/static/js/forum/controllers.js
+++ forum/static/js/forum/controllers.js
@@ -4,10 +4,14 @@
 
 function QuestionCtrl ($scope, $sce, $window, Answer) {
     var questionId = parseInt($window.question_id, 10);
+    var userId = parseInt($window.user_id, 10);
     $scope.answers = Answer.query({question: questionId});
-
     $scope.editor_enabled = true;
-    
+    Answer.query({question: questionId, user: userId}, function(current_user_answer){
+        if (current_user_answer.length != 0) {
+            $scope.editor_enabled = false;
+        }
+    });
     var converter1 = Markdown.getSanitizingConverter();
     $window.converter = converter1;
     var editor1 = new Markdown.Editor(converter1);
