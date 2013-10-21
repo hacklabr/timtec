19958f59c23d9f3abd37dfef22e8f3c635e2166b
Leonardo Santagada <santagada@hacklab.com.br>
2013-10-21 17:06:36 -0200

    reindent

--- tests/confkarma.js
+++ tests/confkarma.js
@@ -1,30 +1,23 @@
 module.exports = function(config){
     config.set({
-    basePath : '../',
-
-    files : [
-      'static/js/vendor/angular.js',
-      'static/js/vendor/angular-*.js',
-      'static/js/vendor/pagedown/*.js',
-      'forum/static/js/truncate.js',
-      'forum/static/js/forum/*.js',
-      'tests/js/lib/*.js',
-      'forum/tests/js/*.js'
-    ],
-
-    autoWatch : true,
-
-    frameworks: ['jasmine'],
-
-    browsers : ['PhantomJS'],
-
-    reporters: ['dots'],
-
-    singleRun: true,
-
-    plugins : [
+        basePath : '../',
+        files : [
+            'static/js/vendor/angular.js',
+            'static/js/vendor/angular-*.js',
+            'static/js/vendor/pagedown/*.js',
+            'forum/static/js/truncate.js',
+            'forum/static/js/forum/*.js',
+            'tests/js/lib/*.js',
+            'forum/tests/js/*.js'
+        ],
+        autoWatch : true,
+        frameworks: ['jasmine'],
+        browsers : ['PhantomJS'],
+        reporters: ['dots'],
+        singleRun: true,
+        plugins : [
             'karma-phantomjs-launcher',
             'karma-jasmine'
-            ]
-
-});};
+        ]
+    });
+};
