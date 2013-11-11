fc64091da74b2cb6de2b0afcd45086f805d18ed6
Bruno Martin <bruno.martin@hacklab.com.br>
2013-11-11 19:05:01 -0200

    course_material admin. Fixes #141. Fixes #153.

--- course_material/static/js/course_material/app.js
+++ course_material/static/js/course_material/app.js
@@ -2,4 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('courseMaterial', ['courseMaterial.services', 'courseMaterial.controllers']);
+angular.module('courseMaterial', ['courseMaterial.services', 'courseMaterial.controllers', 'courseMaterial.directives', 'courseMaterial.filters']);
