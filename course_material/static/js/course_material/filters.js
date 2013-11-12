f31c7ab6516cba393e45565e3bb6f155aeeb5ad9
Leonardo Santagada <santagada@hacklab.com.br>
2013-11-12 19:54:27 -0200

    unfucking js files

--- course_material/static/js/course_material/filters.js
+++ course_material/static/js/course_material/filters.js
@@ -1,10 +1,13 @@
-'use strict';
 
-/* Filters */
+(function (angular, Markdown) {
+    'use strict';
 
-angular.module('courseMaterial.filters', ["ngSanitize"]).
-    filter('markdown', ['$window', function($window) {
-        return function(text) {
-            return text ? Markdown.getSanitizingConverter().makeHtml(text) : "";
-        };
-    }]);
+    /* Filters */
+
+    angular.module('courseMaterial.filters', ["ngSanitize"]).
+        filter('markdown', ['$window', function($window) {
+            return function(text) {
+                return text ? Markdown.getSanitizingConverter().makeHtml(text) : "";
+            };
+        }]);
+})(angular, Markdown);
--- course_material/static/js/course_material/services.js
+++ course_material/static/js/course_material/services.js
@@ -1,14 +1,18 @@
-'use strict';
 
-/* Services */
-    
-angular.module('courseMaterial.services', ['ngRoute', 'ngResource']).
-    factory('CourseMaterial', function($resource){
-        return $resource('/api/course_material/:course/', {}, {
-            update: {method: 'PUT'},
+
+(function (angular) {
+    'use strict';
+
+    /* Services */
+
+    angular.module('courseMaterial.services', ['ngRoute', 'ngResource']).
+        factory('CourseMaterial', function($resource){
+            return $resource('/api/course_material/:course/', {}, {
+                update: {method: 'PUT'}
+            });
+        }).
+        factory('Course', function($resource){
+            return $resource('/api/course/:course_slug/',{});
         });
-    }).
-    factory('Course', function($resource){
-        return $resource('/api/course/:course_slug/',{});
-    });
 
+})(angular);
