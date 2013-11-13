8645f020f9bf302604adf71dffdae2a4f3d3eaee
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2013-11-13 13:02:16 -0200

    reorganize code for closed ticket #155

--- static/js/contact_form.js
+++ static/js/contact_form.js
@@ -1,43 +1,20 @@
+/** require helpers.js */
 
 (function($){
-
-    $.fn.notify = function(text, clazz) {
-        var $div = $('<div style="width:100%;position:fixed;bottom:2em;z-index:1051;text-align:center;">');
-        var $span = $('<span class="alert">').addClass(clazz).html(text);
-        $div.append($span);
-        $(this).css('position','relative').append($div);
-        setTimeout(function(){ $div.remove(); }, 5000);
-    };
-
-    $.fn.asyncSubmit = function(evt) {
-        if(!evt) evt = window.event;
-        if(evt && ('preventDefault' in evt)) {
+    $(function(){
+        $("#modal-contact form:first").submit(function(evt){
+            var $this = $(this);
             evt.preventDefault();
-        }
 
-
-        var $this = $(this);
-        var action = $(this).attr('action');
-        var method = $(this).attr('method');
-        
-        var data = {};
-        $(this).find(':input').each(function(i,e){
-            data[ $(e).attr('name') ] = $(e).val();
+            $this.asyncSubmit().fail(function(){
+                $this.addClass('.has-error');
+                $(document.body).notify('Mensagem não enviada!', 'alert-danger');
+            }).done(function(){
+                $this.removeClass('.has-error');
+                $("#modal-contact").modal("hide");
+                $(document.body).notify('Mensagem enviada!', 'alert-success');
+            });
+            return false;
         });
-
-        request = $.ajax({
-          url: action,
-          type: method,
-          data: data
-        })
-          .fail(function(){
-            $this.addClass('.has-error');
-            $(document.body).notify('Mensagem não enviada!', 'alert-danger');
-        })
-          .done(function(){
-            $this.removeClass('.has-error');
-            $("#modal-contact").modal("hide");
-            $(document.body).notify('Mensagem enviada!', 'alert-success');
-        });
-    };
+    });
 })(jQuery);
