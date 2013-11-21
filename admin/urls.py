d14410f6ba86b95deeefe88df1c0a58388a423fb
Anderson Orui <andersonorui@hacklab.com.br>
2013-11-21 17:52:06 -0200

    starting layout for the users lists

--- admin/urls.py
+++ admin/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     '',
     url(r'^course/(?P<slug>[-a-zA-Z0-9_]+)$', AdminCourseView.as_view(), name='course_admin'),
     url(r'^courses', TemplateView.as_view(template_name="courses.html")),
+    url(r'^users', TemplateView.as_view(template_name="users.html")),
 )
