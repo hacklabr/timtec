0e9656c286854cd4b205b4991a32f4f03edbf15f
Anderson Orui <andersonorui@hacklab.com.br>
2013-11-22 15:42:51 -0200

    deleting admin

--- admin/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from accounts.utils import LoginRequiredMixin
-from django.views.generic.edit import UpdateView
-from core.models import Course
-
-
-class AdminCourseView(LoginRequiredMixin, UpdateView):
-    model = Course
-    template_name = '_base.html'
