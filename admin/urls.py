377d0b29572fd03e582a1ab4e948929ab8efa255
Anderson Orui <andersonorui@hacklab.com.br>
2013-11-22 15:33:11 -0200

    layout

--- admin/urls.py
+++ admin/urls.py
@@ -6,6 +6,7 @@ from views import AdminCourseView
 urlpatterns = patterns(
     '',
     url(r'^course/(?P<slug>[-a-zA-Z0-9_]+)$', AdminCourseView.as_view(), name='course_admin'),
-    url(r'^courses', TemplateView.as_view(template_name="courses.html")),
+    url(r'^courses$', TemplateView.as_view(template_name="courses.html")),
+    url(r'^courses/new', TemplateView.as_view(template_name="new_course.html")),
     url(r'^users', TemplateView.as_view(template_name="users.html")),
 )
