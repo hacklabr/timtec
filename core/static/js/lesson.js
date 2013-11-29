4e31e6f0b659e2fad01c505a1c294d9fdf0da03a
Leonardo Santagada <santagada@gmail.com>
2013-11-29 18:27:32 -0200

    admin, save/load and tons of fixes for html5

--- core/static/js/lesson.js
+++ core/static/js/lesson.js
@@ -1,23 +1,3 @@
-/** TODO: initialize this in proprer way (fabio) */
-function initialize_code_mirror($scope, data, expected) {
-    var body = $('#empty').contents().find('body');
-    var cm = CodeMirror.fromTextArea($('#texto')[0], CodeMirrorConf);
-    cm.setSize("100%", "215px"); // TODO: set size in html
-    cm.markText({line:0, ch:0}, {line:4, ch:0}, {atomic: true, readOnly: true, inclusiveLeft: true});
-    cm.markText({line:4, ch:1000}, {line:7, ch:0}, {atomic: true, readOnly: true, inclusiveRight: true});
-    cm.replaceRange(data, {line:4, ch:0}, {line:4, ch:100});
-    setTimeout(function () {
-        $('#empty').contents().find('body').html(cm.getValue());
-        $('#expected_iframe').contents().find('body').html('' + expected);
-    }, 800);
-    cm.on('change', function (instance) {
-        text= instance.getValue();
-        $('#empty').contents().find('body').html(text);
-        // $scope.answer.given = [text];
-        // $scope.$apply();
-    });
-}
-
 (function (angular) {
     "use strict";
 
@@ -84,8 +64,8 @@ function initialize_code_mirror($scope, data, expected) {
         }
     ]);
 
-    app.controller('LessonActivityCtrl', ['$scope', '$location', '$routeParams', '$http', 'LessonData', 'Answer',
-        function ($scope, $location, $routeParams, $http, LessonData, Answer) {
+    app.controller('LessonActivityCtrl', ['$scope', '$location', '$routeParams', '$http', 'LessonData', 'Answer', '$q',
+        function ($scope, $location, $routeParams, $http, LessonData, Answer, $q) {
             var $main = $scope.$parent;
 
             $main.currentUnitPos = parseInt($routeParams.unitPos, 10);
@@ -127,6 +107,27 @@ function initialize_code_mirror($scope, data, expected) {
                 ga('send', 'event', 'activity', 'submit');
             };
 
+            $scope.emptyLoaded = $q.defer()
+            window.onLoadEmpty = function () {
+                $scope.$apply(function () {
+                    $scope.emptyLoaded.resolve();
+                });
+            }
+            window.onLoadExpected = function () {
+                $('#expected_iframe').contents().find('body').html('' + $scope.currentUnit.activity.expected.expected_answer);
+            }
+
+            $scope.codeMirrorChange = function(text) {
+                $('#empty').contents().find('body').html($scope.answer.given[0]);
+                $scope.answer.given[0] = text;
+            }
+
+            $scope.$watchCollection('answer.given', function () {
+                $scope.emptyLoaded.promise.then(function () {
+                    $('#empty').contents().find('body').html($scope.answer.given[0]);
+                });
+            })
+
             LessonData.then(function (lesson) {
                 var unit = $scope.currentUnit = lesson.units[$main.currentUnitPos - 1];
                 $scope.currentUnitId = unit.id;
@@ -140,8 +141,11 @@ function initialize_code_mirror($scope, data, expected) {
 
                 if(unit.activity.id) {
                     var extractLatest = function (list) {
-                        if(list.length > 0)
+                        if(list.length > 0) {
                             $scope.answer = list.pop();
+                            if(unit.activity.type === "html5")
+                                $scope.loadedAnswer = $scope.answer;
+                        }
                     };
                     Answer.query({'activity': unit.activity.id}, extractLatest);
                 }
@@ -159,16 +163,12 @@ function initialize_code_mirror($scope, data, expected) {
                         $scope.answer.given = unit.activity.data.column1.map(
                             function(a,i){ return null; }
                         );
+                    } else if(unit.activity.type === 'html5') {
+                        var btemplate = "<!DOCTYPE html>\n<html>\n  <head></head>\n  <body>\n";
+                        var atemplate = "\n  </body>\n</html>";
+                        $scope.answer.given = [btemplate + unit.activity.data.data + atemplate];
                     }
                 }
-
-                if(unit.activity.type === 'html5') {
-                    /** TODO: initialize this in proprer way (fabio) */
-                    setTimeout(function () {
-                        initialize_code_mirror($scope, unit.activity.data.data, unit.activity.expected.expected_answer);
-                    }, 100);
-                }
-
             });
         }
     ]);
@@ -326,6 +326,32 @@ function initialize_code_mirror($scope, data, expected) {
                         </label>',
             replace: true
         };
+    });
+
+    app.directive('codemirror', function () {
+        return function (scope, element, attrs) {
+            var cm = CodeMirror.fromTextArea(element[0], CodeMirrorConf);
+            cm.setSize("100%", "215px"); // TODO: set size in html
+            function setValue(value) {
+                cm.setValue(value);
+                cm.markText({line:0, ch:0}, {line:4, ch:0}, {atomic: true, readOnly: true, inclusiveLeft: true});
+                var lastLine = cm.lineCount();
+                cm.markText({line:lastLine-3, ch:1000}, {line:lastLine, ch:0}, {atomic: true, readOnly: true, inclusiveRight: true});
+            }
+            setValue(scope.answer.given[0]);
+
+            scope.$watch('loadedAnswer', function () {
+                if (scope.loadedAnswer) {
+                    setValue(scope.loadedAnswer.given[0]);
+                }
+            })
+            cm.on('change', function (instance) {
+                var text = instance.getValue();
+                scope.$apply(function (scope) {
+                    scope.codeMirrorChange(text);
+                });
+            });
+        }
     });
 
 })(angular);
