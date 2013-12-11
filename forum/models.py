97c93b8af10a1f514870877fc8e32f05608da31a
Bruno Martin <bruno.martin@hacklab.com.br>
2013-12-11 19:16:44 -0200

    fix user model refs, fixes #236

--- forum/models.py
+++ forum/models.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 from django.template.defaultfilters import slugify
 from autoslug import AutoSlugField
 from core.models import Course, Lesson
+from accounts.models import TimtecUser
 
 
 class Question(models.Model):
     title = models.CharField(_('Title'), max_length=255)
     text = models.TextField(_('Question'))
     slug = AutoSlugField(_('Slug'), populate_from='title', max_length=255, editable=False, unique=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'))
+    user = models.ForeignKey(TimtecUser, verbose_name=_('User'))
     correct_answer = models.OneToOneField('Answer', verbose_name=_('Correct answer'), related_name='+', null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
     course = models.ForeignKey(Course, verbose_name=_('Course'))
@@ -33,7 +33,7 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', verbose_name=_('Question'))
     text = models.TextField(_('Answer'))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'), related_name='forum_answers')
+    user = models.ForeignKey(TimtecUser, verbose_name=_('User'), related_name='forum_answers')
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __unicode__(self):
@@ -45,7 +45,7 @@ class Answer(models.Model):
 
 
 class Vote(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'))
+    user = models.ForeignKey(TimtecUser, verbose_name=_('User'))
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
     # Defines vote up or vote down. Vote up:1; Vote down: -1.
     value = models.IntegerField(null=False, blank=False)
