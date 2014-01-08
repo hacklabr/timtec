85caeb608e75195bc629adfea27f8509a2326572
Fabio Montefuscolo <fabio.montefuscolo@hacklab.com.br>
2014-01-08 14:19:20 -0200

    create edit-lesson angular app

---
+++ administration/static/js/edit-lesson/app.js
@@ -0,0 +1,15 @@
+(function(angular){
+    'use strict';
+
+    angular.module('edit-lesson', [
+        'django',
+        // 'directive.alertPopup',
+        // 'directive.contenteditable',
+        'directive.fixedBar',
+        // 'directive.markdowneditor',
+        // 'directive.sortable',
+        // 'filters.text',
+        'ngResource',
+        'youtube'
+    ]);
+})(window.angular);
--- administration/urls.py
+++ administration/urls.py
@@ -8,9 +8,16 @@ urlpatterns = patterns(
         lr(TemplateView.as_view(template_name="new_course.html")),
         name='course_admin'),
 
+    # list all courses
     url(r'^courses/$', lr(TemplateView.as_view(template_name="courses.html"))),
+
+    # create and edit course
     url(r'^courses/new/$', lr(TemplateView.as_view(template_name="new_course.html"))),
     url(r'^courses/(?P<pk>[1-9][0-9]*)/$', lr(TemplateView.as_view(template_name="new_course.html"))),
-    url(r'^courses/(?P<pk>[1-9][0-9]*)/lessons/new/$', lr(TemplateView.as_view(template_name="new_lesson.html"))),
+
+    # create and edit lesson
+    url(r'^courses/(?P<course_id>[1-9][0-9]*)/lessons/new/$', lr(TemplateView.as_view(template_name="new_lesson.html"))),
+    url(r'^courses/(?P<course_id>[1-9][0-9]*)/lessons/(?P<pk>[1-9][0-9]*)/$', lr(TemplateView.as_view(template_name="new_lesson.html"))),
+
     url(r'^users/$', lr(TemplateView.as_view(template_name="users.html"))),
 )
