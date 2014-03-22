f9b642f6fffe6ecc0907234440f6ce341aa2807c
Leonardo Santagada <santagada@gmail.com>
2014-03-22 17:51:54 -0300

    turning off language detection and fixing a bug on angular language settings

--- timtec/settings.py
+++ timtec/settings.py
@@ -411,7 +411,7 @@ AUTH_USER_MODEL = 'accounts.TimtecUser'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
