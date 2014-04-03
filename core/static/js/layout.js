5475a06c1c9dbf3e0c4bafac67f2648834785ffd
Leonardo Santagada <santagada@gmail.com>
2014-04-03 18:48:27 -0300

    fullheight container

--- core/static/js/layout.js
+++ core/static/js/layout.js
@@ -1,15 +1,17 @@
 (function($){
     'use strict';
 
-    function fullHeight(element) {
-        var height = ($('.wrapper').height() - ($('.main-header').height() + $('.main-footer').height()));
-        if ($('.wrapper').width() > 992) {
+    function fullHeight(element, extra) {
+        if(!extra) extra = 0;
+        var height = ($('.wrapper').height() - ($('.main-header').height() + extra + $('.main-footer').height()));
+        if ($('.wrapper').height() > 780) {
             $(element).height(height);
         }
     }
 
     $(function () {
         fullHeight('.js-fullheight');
+        fullHeight('.js-fullheight-container', 85);
     });
 
 })(window.jQuery);
