cf5aec0ef5b4161226ba1b18f05c4e83f2802d07
Bruno Martin <bruno.martin@hacklab.com.br>
2014-04-05 18:10:44 -0300

    fix loading activity after video

--- core/static/js/lesson/controllers.js
+++ core/static/js/lesson/controllers.js
@@ -157,3 +157,53 @@
     ]);
 
 })(angular);
+
+
+(function() {
+    var _pauseFlag = false;
+    var start, whole;
+
+    var lastState = -1;
+
+    function  onPlayerStateChange (event) {
+        var video_id = event.target.getVideoData().video_id;
+
+        if (event.data == YT.PlayerState.ENDED){
+            if(! (lastState === YT.PlayerState.PAUSED ||   // workaround, YT in html5 mode will fire
+                  lastState === YT.PlayerState.PLAYING)) { // event with ENDED state after cue video.
+                event.data = lastState;
+            } else {
+                ga('send', 'event', 'videos', 'watch To end', video_id);
+                if (whole === 'started') {
+                    var stop = new Date().getTime();
+                    var delta_s = (stop - start) / 1000;
+                    ga('send', 'event', 'videos', 'time tO end', video_id, Math.round(delta_s));
+                    whole = 'ended';
+                }
+            }
+        }
+
+        if (event.data == YT.PlayerState.PLAYING){
+                ga('send', 'event', 'videos', 'play', video_id);
+                _pauseFlag = false;
+                if (whole !== 'ended' && whole !== 'started') {
+                    start = new Date().getTime();
+                    whole = 'started';
+                }
+        }
+
+        if (event.data == YT.PlayerState.PAUSED && _pauseFlag === false){
+            ga('send', 'event', 'videos', 'pause', video_id);
+            _pauseFlag = true;
+        }
+        if (event.data == YT.PlayerState.BUFFERING){
+            ga('send', 'event', 'videos', 'bufferIng', video_id);
+        }
+        if (event.data == YT.PlayerState.CUED){
+            ga('send', 'event', 'videos', 'cueing', video_id);
+        }
+
+        lastState = event.data;
+    }
+    window.onPlayerStateChange = onPlayerStateChange;
+})();
