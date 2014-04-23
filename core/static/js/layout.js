3253afe6bcd66b0c57d27ca8d8ba71df9ec0eeeb
Leonardo Santagada <santagada@gmail.com>
2014-04-23 19:24:34 -0300

    cache and when two columns are one over the other don't make them as tall as the page

--- core/static/js/layout.js
+++ core/static/js/layout.js
@@ -1,25 +1,44 @@
 (function($){
     'use strict';
 
+    var oldMax = 0;
+    var oldVisible = 0;
+    var oldWindowWidth = 0;
+
     function res () {
         var $visibleFull = $('.js-fullheight:visible');
         var heights = $visibleFull.map(function () {
             return $(this).height();
         }).get();
-
+        var lefts = $visibleFull.map(function () {
+            return $(this).offset().left;
+        }).get();
         var footerpos = $('.main-footer').offset().top  -  $('header.main-header').height() + 5; // Discover where does this 5 pixels come from
         heights.push(footerpos);
         var tallerSize = Math.max.apply(null, heights);
 
-        console.log($visibleFull.length);
-        console.log(tallerSize, footerpos);
+        // cache: only run if things changed since last time
+        if((oldMax === tallerSize) &&
+           (oldVisible === $visibleFull.length) &&
+           (oldWindowWidth === $(window).width())) {
+            return;
+        } else {
+            oldMax = tallerSize;
+            oldVisible = $visibleFull.length;
+            oldWindowWidth = $(window).width();
+        }
 
-        function positionBack (sel1, sel2) {
+        console.log('v', oldVisible, 't', oldMax, 'f', footerpos);
+
+        function positionBack (sel1, sel2, makeTall) {
             var $b1 = $(sel1);
             var $c1 = $(sel2 + ':visible');
             if($c1.length > 0) {
                 $b1.show();
-                $b1.height(tallerSize);
+                if(makeTall)
+                    $b1.height(tallerSize)
+                else
+                    $b1.height($c1.outerHeight())
                 $b1.width($c1.outerWidth());
                 $b1.offset($c1.offset());
             } else {
@@ -27,8 +46,12 @@
             }
         }
 
-        positionBack('.b1', '.c1');
-        positionBack('.b2', '.c2');
+        var makeTall = true;
+        if((oldVisible == 2) && (lefts[0] === lefts[1]))
+            makeTall = false;
+        console.log('mt', makeTall);
+        positionBack('.b1', '.c1', makeTall);
+        positionBack('.b2', '.c2', makeTall);
     }
 
     $(function () {
