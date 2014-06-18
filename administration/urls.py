7137d9420a1ab646ac3a1d04021e51734a2ac550
Leonardo Santagada <santagada@gmail.com>
2014-06-18 15:08:47 -0300

    activate new template layout
    administration templates goes in the administration forlder

--- administration/urls.py
+++ administration/urls.py
@@ -9,15 +9,15 @@ urlpatterns = patterns(
     '',
     # list all courses
     url(r'^$', lr(RedirectView.as_view(url="courses/")), name="administration.home"),
-    url(r'^courses/$', AdminView.as_view(template_name="courses_admin.html")),
+    url(r'^courses/$', AdminView.as_view(template_name="courses.html")),
 
     # create and edit course
     url(r'^courses/new/$', AdminView.as_view(template_name="course.html")),
     url(r'^courses/(?P<pk>[1-9][0-9]*)/$', AdminView.as_view(template_name="course.html")),
 
     # create and edit lesson
-    url(r'^courses/(?P<course_id>[1-9][0-9]*)/lessons/new/$', AdminView.as_view(template_name="new_lesson.html")),
-    url(r'^courses/(?P<course_id>[1-9][0-9]*)/lessons/(?P<pk>[1-9][0-9]*)/$', AdminView.as_view(template_name="new_lesson.html")),
+    url(r'^courses/(?P<course_id>[1-9][0-9]*)/lessons/new/$', AdminView.as_view(template_name="lesson.html")),
+    url(r'^courses/(?P<course_id>[1-9][0-9]*)/lessons/(?P<pk>[1-9][0-9]*)/$', AdminView.as_view(template_name="lesson.html")),
 
     # messages
     url(r'^course/(?P<course_id>[1-9][0-9]*)/messages/$', AdminView.as_view(template_name="messages.html")),
