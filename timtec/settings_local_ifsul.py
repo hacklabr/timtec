3771a982ef45590ebf13d494be0e3e0a22a8b588
Leonardo Santagada <santagada@gmail.com>
2014-08-25 17:57:20 -0300

    ifsul deployment

---
+++ timtec/settings_local_ifsul.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# configurations for the production server
+# https://docs.djangoproject.com/en/dev/ref/settings/
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+TIMTEC_THEME = 'default'
+
+SITE_ID = 2
+
+ALLOWED_HOSTS = [
+    'ifsul.timtec.com.br',
+    '.ifsul.timtec.com.br',
+    'ifsul.hacklab.com.br',
+    '.ifsul.hacklab.com.br',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timtec-ifsul',
+        'USER': 'timtec-ifsul',
+    }
+}
+
+MEDIA_ROOT = "/home/timtec-ifsul/webfiles/media/"
+STATIC_ROOT = "/home/timtec-ifsul/webfiles/static/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_SUBJECT_PREFIX = '[timtec-ifsul]'
+DEFAULT_FROM_EMAIL = 'donotreply@m.timtec.com.br'
+CONTACT_RECIPIENT_LIST = ['timtec-dev@listas.hacklab.com.br', ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'logfile': {
+            'level': 'WARN',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/home/timtec-ifsul/django.log',
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['logfile'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+try:
+    # THIS FILE SETS PRODUCTION SPECIFIC
+    # SENSITIVY VALUES (SUCH AS GOOGLE ANALYTICS KEY)
+    from .settings_production import *
+except ImportError:
+    pass
