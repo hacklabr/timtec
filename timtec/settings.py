fc878d0650eea02c701c7bc6f2a7f3c2eb27f77d
Bruno Martin <bruno.martin@hacklab.com.br>
2014-08-26 10:56:05 -0300

    fix debug tool bar related issue

--- timtec/settings.py
+++ timtec/settings.py
@@ -548,3 +548,12 @@ if DEBUG:
     DEBUG_TOOLBAR_CONFIG = {
         'INTERCEPT_REDIRECTS': False,
     }
+
+# Fix debug toolbar issue: https://github.com/django-debug-toolbar/django-debug-toolbar/issues/521
+try:
+    if sys.argv[1] == 'runserver' or sys.argv[1] == 'runserver_plus':
+        DEBUG_TOOLBAR_PATCH_SETTINGS = DEBUG
+    else:
+        DEBUG_TOOLBAR_PATCH_SETTINGS = False
+except IndexError:
+        DEBUG_TOOLBAR_PATCH_SETTINGS = False
