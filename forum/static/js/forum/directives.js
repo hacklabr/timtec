25df9bf1d65dfb87bbcbfa0c5b6cafc1fe02a72e
Leonardo Santagada <santagada@gmail.com>
2014-09-16 20:27:04 -0300

    markdown editor doesn't work on touch screens

--- forum/static/js/forum/directives.js
+++ forum/static/js/forum/directives.js
@@ -9,9 +9,10 @@
                 "controller": ['$scope', '$element', function($scope, $element) {
                     $element.find('textarea').attr('id', "wmd-input");
                     $element.find('.js-button-bar').attr('id', "wmd-button-bar");
-
-                    var editor = new Markdown.Editor(Markdown.getSanitizingConverter());
-                    editor.run();
+                    if (!Modernizr.touch) {
+                        var editor = new Markdown.Editor(Markdown.getSanitizingConverter());
+                        editor.run();
+                    }
                 }],
                 "link": function(scope, element) {
                     var read = function read(evt){
