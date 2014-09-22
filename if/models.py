edf1d6bb18dbcb3a43657c518289f272d1a3323d
Bruno Martin <bruno.martin@hacklab.com.br>
2014-09-22 17:37:00 -0300

    working signup form, with validation

--- if/models.py
+++ if/models.py
@@ -1,15 +1,15 @@
 # coding=utf-8
 from django.db import models
-from accounts.models import TimtecUser
+from accounts.models import BaseTimtecUser
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import UserManager
 
 
-class IfUser(TimtecUser):
+class IfUser(BaseTimtecUser):
 
     # students fields
     # if_id eh o identificador de matricula
-    if_id = models.CharField(_('Course'), max_length=30, blank=True)
+    ifid = models.CharField(_('Academic Identificator'), max_length=30, blank=True)
     course = models.CharField(_('Course'), max_length=30, blank=True)
     klass = models.CharField(_('Class'), max_length=30, blank=True)
 
