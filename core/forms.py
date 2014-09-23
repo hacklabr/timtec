7b5f1ea4cac8b3ae9e4e5e3bfa547d6060892ebe
Leonardo Santagada <santagada@gmail.com>
2014-09-23 18:39:34 -0300

    fix test errors

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
-from .models import Class, CourseStudent
+from .models import Class
 import logging
 
 logger = logging.getLogger(__name__)
