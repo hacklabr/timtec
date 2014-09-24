26baaa919bf8b31f9b6b8b597ae5c8b1e7175cc8
Bruno Martin <bruno.martin@hacklab.com.br>
2014-09-24 00:15:44 -0300

    fix if model

--- if/models.py
+++ if/models.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 from django.db import models
-from accounts.models import BaseTimtecUser
+from accounts.models import TimtecUser
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import UserManager
 
 
-class IfUser(BaseTimtecUser):
+class IfUser(TimtecUser):
 
     # students fields
     # if_id eh o identificador de matricula
