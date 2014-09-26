f014f82a75980f9b742cb7db27834e30098a895f
Bruno Martin <bruno.martin@hacklab.com.br>
2014-09-26 11:16:33 -0300

    add default group to imported users

--- core/management/commands/import_students.py
+++ core/management/commands/import_students.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.conf import settings
 
 import unicodecsv
 
@@ -11,6 +15,11 @@ class Command(BaseCommand):
     help = 'import users'
 
     def handle(self, *args, **options):
+        try:
+            default_group = Group.objects.get(name=settings.REGISTRATION_DEFAULT_GROUP_NAME)
+        except ObjectDoesNotExist:
+            print "Grupo padrão não configurado. Verifique a variável " \
+                  "REGISTRATION_DEFAULT_GROUP_NAME no arquivo settings.py e se o grupo exite"
         with open('../ifsul.csv', 'r') as csvfile:
             readf = unicodecsv.DictReader(csvfile)
             for row in readf:
@@ -21,4 +30,6 @@ class Command(BaseCommand):
                         row[i] = row[i][:30]
                 nu = User.objects.create(**row)
                 nu.set_password('x')
+                nu.groups.add(default_group)
+                nu.is_if_staff = True
                 nu.save()
