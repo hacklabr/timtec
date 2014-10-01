086d6a66aa3edbf809052987f56a01fe2cec6107
Leonardo Santagada <santagada@gmail.com>
2014-10-01 16:49:42 -0300

    better comments and autoload virtualenv and cd

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -4,14 +4,21 @@ TIMTEC_USER=vagrant
 
 # useradd -U -m ${TIMTEC_USER}
 
+## Install OS dependencies
+
 sudo apt-get update
 sudo apt-get install -y libpq-dev libjpeg-dev libpng12-dev build-essential python-dev gettext python-virtualenv nodejs npm git
 
+# Ubuntu name the node binary nodejs
 sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+
+## Database setup
 
 sudo apt-get install -y postgresql
 sudo su - postgres -c "createuser -d ${TIMTEC_USER}"
 createdb timtec
 
 ./timtec/scripts/env-setup.sh
-./timtec/scripts/production-ubuntu.sh
+
+echo "source timtec-env/bin/activate" >> ~/.bashrc
+echo "cd timtec" >> ~/.bashrc
