b389edee38c1cf89a53a87ba6c65e9c437718628
Bruno Martin <bruno.martin@hacklab.com.br>
2014-10-18 23:18:17 -0300

    classes link in admin

--- administration/static/js/new-course/controllers.js
+++ administration/static/js/new-course/controllers.js
@@ -33,6 +33,7 @@
                         $scope.forum_url = 'admin/course/' + course.id +  '/forum/';
                         $scope.messages_url = 'admin/course/' + course.id   + '/messages/';
                         $scope.reports_url = 'admin/course/' + course.id   + '/stats/';
+                        $scope.classes_url = 'course/' + course.slug + '/classes/';
                         $scope.permissions_url = 'admin/course/' + course.id + '/permissions/';
                     })
                     .then(function(){
