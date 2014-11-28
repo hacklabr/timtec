45b32e24bf88045743d4adcf8623aa4722bd86d8
Rafael Monico <rmonico@hacklab.com.br>
2014-11-28 20:22:23 -0200

    Tests of ClassUpdateView (not passing yet)

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.test.client import Client
+
 import pytest
 from model_mommy import mommy
+
+User = get_user_model()
 
 
 @pytest.mark.django_db
@@ -8,3 +13,35 @@ def test_lesson(admin_client):
     response = admin_client.get('/course/' + lesson.course.slug + '/lesson/' + lesson.slug + '/')
     assert response.status_code == 200
     assert lesson.name.encode('utf-8') in response.content
+
+
+def create_professor(course, professor_user_name, professor_email, role):
+    professor = User.objects.create_user(professor_user_name, professor_email, 'password')
+
+    mommy.make('CourseProfessor', user=professor, course=course)
+
+    non_coordinator_course_professor = course.courseprofessor_set.filter(user=professor)
+    non_coordinator_course_professor.role = role
+
+    return professor
+
+
+@pytest.mark.django_db
+def test_assistant_professor_shouldnot_change_class_professor(admin_client):
+
+    course = mommy.make('Course', slug='dbsql', name='Test course name')
+
+    coordinator_professor = create_professor(course, 'coordinator_professor', 'coordinator_professor@test.com', 'coordinator')
+
+    assistant_professor = create_professor(course, 'assistant_professor', 'assistant_professor@test.com', 'assistant')
+
+    another_assistant = create_professor(course, 'another_assistant', 'another_assistant@test.com', 'assistant')
+
+    clazz = mommy.make('Class', name='Test class name', course=course, assistant=coordinator_professor)
+
+    client = Client()
+    client.login(username=assistant_professor.username, password='password')
+
+    # post passing new_assistant as new assistant
+    response = admin_client.post('/class/' + str(clazz.id) + '/', {'assistant': another_assistant})
+    assert response.status_code == 403
