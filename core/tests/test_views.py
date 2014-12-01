208d1f1d3c27170b37c0b4dced857e521594e98c
Rafael Monico <rmonico@hacklab.com.br>
2014-12-01 18:41:00 -0200

    New test: test_coordinator_professor_should_can_change_class_professor (passing)

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -26,7 +26,7 @@ def create_professor(course, professor_user_name, professor_email, role):
 
 
 @pytest.mark.django_db
-def test_assistant_professor_shouldnot_change_class_professor(client):
+def test_assistant_professor_should_cannot_change_class_professor(client):
 
     course = mommy.make('Course', slug='dbsql', name='Test course name')
 
@@ -39,6 +39,26 @@ def test_assistant_professor_shouldnot_change_class_professor(client):
     clazz = mommy.make('Class', name='Test class name', course=course, assistant=coordinator_professor)
 
     client.login(username=assistant_professor.username, password='password')
+
+    # post passing new_assistant as new assistant
+    response = client.post('/class/' + str(clazz.id) + '/', {'assistant': another_assistant})
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_coordinator_professor_should_can_change_class_professor(client):
+
+    course = mommy.make('Course', slug='dbsql', name='Test course name')
+
+    coordinator_professor = create_professor(course, 'coordinator_professor', 'coordinator_professor@test.com', 'coordinator')
+
+    assistant_professor = create_professor(course, 'assistant_professor', 'assistant_professor@test.com', 'assistant')
+
+    another_assistant = create_professor(course, 'another_assistant', 'another_assistant@test.com', 'assistant')
+
+    clazz = mommy.make('Class', name='Test class name', course=course, assistant=assistant_professor)
+
+    client.login(username=coordinator_professor.username, password='password')
 
     # post passing new_assistant as new assistant
     response = client.post('/class/' + str(clazz.id) + '/', {'assistant': another_assistant})
