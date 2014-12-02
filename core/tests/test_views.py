2ebcd020caf02c865e5f28da8027a0164eef2c51
Rafael Monico <rmonico@hacklab.com.br>
2014-12-02 16:25:03 -0200

    New test: test_coordinator_professor_should_can_change_class_professor

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth import get_user_model
-
+# -*- coding: utf-8 -*-
 import pytest
 from model_mommy import mommy
 
-User = get_user_model()
+from conftest import create_user
 
 
 @pytest.mark.django_db
@@ -14,52 +13,59 @@ def test_lesson(admin_client):
     assert lesson.name.encode('utf-8') in response.content
 
 
-def create_professor(course, professor_user_name, professor_email, role):
-    professor = User.objects.create_user(professor_user_name, professor_email, 'password')
+def assign_professor_to_course(course, existing_professor=None, new_professor_username=None, role=None):
+    """
 
-    mommy.make('CourseProfessor', user=professor, course=course)
+    :param existing_professor: Existing object TimtecUser
+    :param new_professor_user_name: Non-existing professor username, if not defined, it will be created
+    :param course: The course to assign professor to
+    :param role: Role to assign to professor
+    :return: The created/existing professor
+    """
 
-    non_coordinator_course_professor = course.courseprofessor_set.filter(user=professor)
-    non_coordinator_course_professor.role = role
+    if existing_professor is not None:
+        professor = existing_professor
+    else:
+        professor = create_user(new_professor_username)
+
+    mommy.make('CourseProfessor', user=professor, course=course, role=role)
 
     return professor
 
 
 @pytest.mark.django_db
 def test_assistant_professor_should_cannot_change_class_professor(client):
-
     course = mommy.make('Course', slug='dbsql', name='Test course name')
 
-    coordinator_professor = create_professor(course, 'coordinator_professor', 'coordinator_professor@test.com', 'coordinator')
+    coordinator_professor = assign_professor_to_course(course, new_professor_username='coordinator_professor', role='coordinator')
 
-    assistant_professor = create_professor(course, 'assistant_professor', 'assistant_professor@test.com', 'assistant')
+    assistant_professor = assign_professor_to_course(course, new_professor_username='assistant_professor', role='assistant')
 
-    another_assistant = create_professor(course, 'another_assistant', 'another_assistant@test.com', 'assistant')
+    another_assistant = assign_professor_to_course(course, new_professor_username='another_assistant', role='assistant')
 
     clazz = mommy.make('Class', name='Test class name', course=course, assistant=coordinator_professor)
 
     client.login(username=assistant_professor.username, password='password')
 
-    # post passing new_assistant as new assistant
     response = client.post('/class/' + str(clazz.id) + '/', {'assistant': another_assistant})
+
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
 def test_coordinator_professor_should_can_change_class_professor(client):
-
     course = mommy.make('Course', slug='dbsql', name='Test course name')
 
-    coordinator_professor = create_professor(course, 'coordinator_professor', 'coordinator_professor@test.com', 'coordinator')
+    coordinator_professor = assign_professor_to_course(course, new_professor_username='coordinator_professor', role='coordinator')
 
-    assistant_professor = create_professor(course, 'assistant_professor', 'assistant_professor@test.com', 'assistant')
+    assistant_professor = assign_professor_to_course(course, new_professor_username='assistant_professor', role='assistant')
 
-    another_assistant = create_professor(course, 'another_assistant', 'another_assistant@test.com', 'assistant')
+    another_assistant = assign_professor_to_course(course, new_professor_username='another_assistant', role='assistant')
 
     clazz = mommy.make('Class', name='Test class name', course=course, assistant=assistant_professor)
 
     client.login(username=coordinator_professor.username, password='password')
 
-    # post passing new_assistant as new assistant
     response = client.post('/class/' + str(clazz.id) + '/', {'assistant': another_assistant})
-    assert response.status_code == 403
+
+    assert response.status_code == 200
