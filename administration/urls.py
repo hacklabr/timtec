ac03c9d2b81e044e894f2cf181e5a4d214d845f7
Bruno Martin <bruno.martin@hacklab.com.br>
2014-12-15 15:14:48 -0200

    fix users admin link in menu header

--- administration/urls.py
+++ administration/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns(
     url(r'^home/$', lr(AdminView.as_view(template_name="home.html")), name="administration.home"),
 
     # list all courses
-    url(r'^$', lr(RedirectView.as_view(url="courses/")), name="administration.home"),
+    url(r'^$', lr(RedirectView.as_view(url="courses/"))),
     url(r'^courses/$', AdminView.as_view(template_name="courses.html"), name='administration.courses'),
 
     # users
