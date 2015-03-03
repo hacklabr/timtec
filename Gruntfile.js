d49d3c5e183e33d158c38a4827158695386e76b8
Gutieri Barboza <eugutierri@gmail.com>
2015-03-03 15:13:50 -0300

    configuration of GruntJS for compilation less and watch less

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,14 +15,44 @@ module.exports = function (grunt) {
                 }
             },
         },
+
+        less: {
+          development: {
+            files: {
+              "themes/new-if/static/css/main.css": "themes/new-if/static/css/main.less"
+           }
+          }
+        },
+
+        watch: {
+          styles: {
+            // Which files to watch (all .less files recursively in the less directory)
+            files: [
+                'themes/new-if/static/css/less/**/*.less'
+            ],
+            tasks: ['less'],
+            options: {
+              nospawn: true
+            }
+          }
+        }
+
     });
 
     // Load tasks so we can use them
     grunt.loadNpmTasks('grunt-angular-gettext');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // by default we call compile
     grunt.registerTask('default', ['nggettext_compile']);
 
     grunt.registerTask('compile', ['nggettext_compile']);
     grunt.registerTask('extract', ['nggettext_extract']);
+
+    // Watch changes on files *.less and compile Less
+    grunt.registerTask('less-watch', ['less', 'watch']);
+
+    // Compile Less
+    grunt.registerTask('less-compile', ['less']);
 };
