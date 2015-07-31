e1cbe1211c77a20494cfecca43bf310ecb588404
Bruno Martin <bruno.martin@hacklab.com.br>
2015-07-31 00:30:23 -0300

    fix course import and export

--- administration/serializer.py
+++ administration/serializer.py
@@ -1,18 +1,25 @@
 from core.models import Course, CourseAuthor, Lesson, Unit
-from core.serializers import VideoSerializer, CourseAuthorSerializer
+from core.serializers import VideoSerializer
 from activities.serializers import ActivityImportExportSerializer
 from course_material.serializers import CourseMaterialImportExportSerializer
 from rest_framework import serializers
 
 
-class CourseAtuhorsExportSerializer(serializers.ModelSerializer):
+class CourseAuthorsExportSerializer(serializers.ModelSerializer):
     name = serializers.Field(source='get_name')
     biography = serializers.Field(source='get_biography')
     picture = serializers.Field(source='get_picture_url')
 
     class Meta:
         model = CourseAuthor
-        exclude = ('pk', 'user',)
+        exclude = ('id', 'user', 'course',)
+
+
+class CourseAuthorsImportSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CourseAuthor
+        exclude = ('id', 'user', 'course',)
 
 
 class UnitImportExportSerializer(serializers.ModelSerializer):
@@ -21,7 +28,7 @@ class UnitImportExportSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Unit
-        exclude = ('pk',)
+        exclude = ('id', 'lesson',)
 
 
 class LessonImportExportSerializer(serializers.ModelSerializer):
@@ -29,12 +36,12 @@ class LessonImportExportSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
-        exclude = ('pk',)
+        exclude = ('id', 'course',)
 
 
 class CourseExportSerializer(serializers.ModelSerializer):
     lessons = LessonImportExportSerializer(many=True, allow_add_remove=True)
-    course_authors = CourseAtuhorsExportSerializer(many=True,
+    course_authors = CourseAuthorsExportSerializer(many=True,
                                                    allow_add_remove=True,)
     intro_video = VideoSerializer()
     course_material = CourseMaterialImportExportSerializer()
@@ -48,12 +55,12 @@ class CourseExportSerializer(serializers.ModelSerializer):
 
 class CourseImportSerializer(serializers.ModelSerializer):
     lessons = LessonImportExportSerializer(many=True, allow_add_remove=True)
-    course_authors = CourseAuthorSerializer(many=True, allow_add_remove=True)
+    course_authors = CourseAuthorsImportSerializer(many=True, allow_add_remove=True)
     intro_video = VideoSerializer()
-    course_material = CourseMaterialImportExportSerializer()
+    # course_material = CourseMaterialImportExportSerializer()
 
     class Meta:
         model = Course
         fields = ('slug', 'name', 'intro_video', 'application', 'requirement', 'abstract', 'structure',
                   'workload', 'pronatec', 'status', 'thumbnail', 'home_thumbnail', 'home_position',
-                  'start_date', 'home_published', 'course_authors', 'lessons', 'course_material',)
+                  'start_date', 'home_published', 'course_authors', 'lessons',)
