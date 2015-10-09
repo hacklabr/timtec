ad8c69ee259b872aad7f718711aee6dd5e020fd1
Bruno Martin <bruno.martin@hacklab.com.br>
2015-10-09 15:56:58 -0300

    merge migrations

---
+++ core/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_auto_20151003_2000'),
+        ('core', '0003_class_user_can_certificate'),
+    ]
+
+    operations = [
+    ]
