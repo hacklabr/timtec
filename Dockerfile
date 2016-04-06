32b3810c4abfba39d5bb6573d082ee0a2708b23d
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2016-04-06 17:18:57 -0300

    preparing for docker

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+FROM debian:jessie
+MAINTAINER Fabio Montefuscolo <fabio.montefuscolo@hacklab.com.br>
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update \
+    && apt-get install -y python python-pip python-dev \
+    && apt-get install -y libpq-dev libjpeg-dev libpng12-dev gettext \
+    && apt-get install -y nginx supervisor \
+    && apt-get install -y npm git
+
+RUN ln -s /usr/bin/nodejs /usr/bin/node
+RUN npm install -g phantomjs-prebuilt bower
+
+RUN useradd -m -d /app -u 1000 -s /bin/bash timtec
+RUN mkdir -p /app
+
+# Prefer to use mdillon/postgis
+ENV DJANGO_DATABASE_URL='postgres://timtec:timtec@postgres/timtec'
+ENV DJANGO_SETTINGS_MODULE='timtec.settings_local_docker'
+ENV PYTHONPATH='/app'
+ENV GUNICORN_LOG_LEVEL='info'
+ENV GUNICORN_EXTRA_FLAGS=''
+
+COPY . /app/timtec
+RUN mkdir -p /app/webfiles/static \
+   && mkdir -p /app/webfiles/media \
+   && pip install -r /app/timtec/requirements/production.txt \
+   && chown -R timtec /app
+
+WORKDIR /app/timtec
+USER timtec
+
+RUN npm install \
+   && node bower install \
+   && node node_modules/grunt-cli/bin/grunt \
+   && python manage.py collectstatic --noinput \
+   && python manage.py compilemessages
+
+USER root
+EXPOSE 80 8000
+ENTRYPOINT  ["/app/timtec/docker/entrypoint.py"]
+CMD ["supervisord", "-c", "/app/timtec/docker/supervisord.conf"]
---
+++ docker/entrypoint.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import time
+import django
+import environ
+import psycopg2
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+sys.path.insert(1, '/app/timtec')
+env = environ.Env()
+environ.Env.read_env('/app/.env')
+
+command = ['supervisord', '-c', '/app/timtec/docker/supervisord.conf']
+database = settings.DATABASES.get('default')
+
+if sys.argv[1:] != command:
+    subprocess.call(sys.argv[1:])
+    sys.exit(0)
+
+tries = 5
+timeout = 5
+exception = None
+
+#
+# Waiting for postgis start on docker-compose
+#
+while tries > 0:
+    try:
+        conn = psycopg2.connect(
+            user='postgres',
+            host='postgres',
+            password=env('POSTGRES_ENV_POSTGRES_PASSWORD')
+        )
+        tries = 0
+        exception = None
+    except psycopg2.OperationalError as e:
+        exception = e
+        tries = tries - 1
+        sys.stderr.write('Failed to connect to postgres, %d tries reamaining...\n' % tries)
+        time.sleep(timeout)
+
+if exception:
+    raise exception
+
+conn.set_isolation_level(0)
+cur = conn.cursor()
+
+cur.execute("SELECT count(1) > 0 FROM pg_user WHERE usename='%(USER)s';" % database)
+user_exists = cur.fetchone()[0]
+
+cur.execute("SELECT count(1) FROM pg_database WHERE datname='%(NAME)s';" % database)
+db_exists = cur.fetchone()[0]
+
+if not user_exists:
+    cur.execute("CREATE USER %(USER)s WITH PASSWORD '%(PASSWORD)s';" % database)
+
+if not db_exists:
+    cur.execute("CREATE DATABASE %(NAME)s OWNER %(USER)s;" % database)
+    cur.close()
+    conn.close()
+
+    conn = psycopg2.connect(
+        dbname=database.get('NAME'),
+        user='postgres',
+        host='postgis',
+        password=env('POSTGIS_ENV_POSTGRES_PASSWORD')
+    )
+    conn.set_isolation_level(0)
+    cur = conn.cursor()
+
+cur.close()
+conn.close()
+
+execute_from_command_line(['manage.py', 'migrate'])
+execute_from_command_line(['manage.py', 'loaddata', 'initial'])
+
+subprocess.call(sys.argv[1:])
