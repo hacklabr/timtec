85c934cdcd6c523ca323d7d36c9c829239edfc75
Bruno Martin <bruno.martin@hacklab.com.br>
2016-07-10 23:38:20 -0300

    remove unused MANAGERS variable from settings.py

--- timtec/settings.py
+++ timtec/settings.py
@@ -26,8 +26,6 @@ ADMINS = (
     ('Admin1', 'root@localhost'),
 )
 
-MANAGERS = (ADMINS[1],)
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'localhost'
 DEFAULT_FROM_EMAIL = 'donotreply-dev@m.timtec.com.br'
