84c6ebaa87c9675f972a22a682d208efc24e5284
Bruno Martin <bruno.martin@hacklab.com.br>
2016-07-10 23:31:52 -0300

    remove duplicated context processor

--- timtec/settings.py
+++ timtec/settings.py
@@ -569,7 +569,6 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
