23f65e8e541accb4ed1af63d19dde026b783ae16
Bruno Martin <bruno.martin@hacklab.com.br>
2016-08-01 04:44:50 -0300

    limit courses to 2

--- core/static/js/dashboard/dashboard-controllers.js
+++ core/static/js/dashboard/dashboard-controllers.js
@@ -2,9 +2,16 @@
     'use strict';
     var app = angular.module('dashboard.controllers', []);
 
-    app.controller('DashboardCtrl', ['$scope', 'CourseProfessor', 'Class',
-        function ($scope, CourseStudentService) {
-
+    app.controller('DashboardCtrl', ['$scope', 'CourseStudent', 'Topic',
+        function ($scope, CourseStudent, Topic) {
+            CourseStudent.query({}, function(my_courses){
+                if (my_courses.length > 2) {
+                    $scope.my_courses = my_courses.slice(0, 2);
+                } else {
+                    $scope.my_courses = my_courses;
+                }
+            });
+            $scope.latest_topics = Topic.query({limit: 12, ordering: 'updated_at'})
         }
     ]);
 
