155ce2943b713cc705a046b69ebac1e2b406a2ec
Laury Bueno <laury@hacklab.com.br>
2016-09-23 11:57:21 -0300

    Dependencies of activity controller reorganized

--- activities/static/js/activities/app.js
+++ activities/static/js/activities/app.js
@@ -1,5 +1,11 @@
 (function(angular){
     'use strict';
 
-    angular.module('activities', ['django', 'activities.controllers', 'activities.directives', 'ui.bootstrap', 'ui.codemirror']);
+    angular.module('activities', [
+      'django',
+      'activities.controllers',
+      'activities.directives',
+      'ui.bootstrap',
+      'ui.codemirror'
+    ]);
 })(angular);
