35e61ff20b3603774c20702794ed01a0d8410b78
Moacir Moda <moa.moda@gmail.com>
2016-11-24 16:21:48 -0200

    merging some fixtures from 3.3 and 4.0

---
+++ activities/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('activities', '0002_auto_20160923_1847'),
+        ('activities', '0003_auto_20160730_1743'),
+    ]
+
+    operations = [
+    ]
