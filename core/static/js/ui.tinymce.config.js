56cc709c1726593e3415fc0281f2ef7a28b04d01
Laury Bueno <laury@hacklab.com.br>
2016-12-16 15:14:43 -0200

    TinyMCE agora suporta vídeos do Youtube (refs hacklabr/paralapraca#225)

--- core/static/js/ui.tinymce.config.js
+++ core/static/js/ui.tinymce.config.js
@@ -1,7 +1,7 @@
 (function(angular){
     'use strict';
 
-    var app = angular.module('ui.tinymce')
+    var app = angular.module('ui.tinymce');
 
     // If you want to cutomize uiTinymceConfig for your theme, please copy this file to you theme in the
     // same path (static/js/). This are the default setting for timtec
@@ -18,6 +18,22 @@
         theme : 'modern',
         language: 'pt_BR',
         language_url : '/static/vendor/tinymce/langs/pt_BR.js',
+
+        // media customizations
+        media_poster: false,
+        media_alt_source: false,
+        media_dimensions: false,
+        media_url_resolver: function (data, resolve/*, reject*/) {
+            var complete_url = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var result = complete_url.exec(data.url);
+            if (result && result[2].length == 11) {
+                var youtube_id = result[2];
+                var embedHtml = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ youtube_id +'" frameborder="0" allowfullscreen></iframe>';
+                resolve({html: embedHtml});
+            } else {
+                resolve({html: ''});
+            }
+        },
     })
 
 })(window.angular);
