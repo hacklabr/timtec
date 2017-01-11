a51230762b0186cb68080ce93af11868fb7b683c
Laury Bueno <laury@hacklab.com.br>
2017-01-11 11:43:50 -0200

    Slidesreveal directive improved (refs hacklabr/paralaprca#314)

--- activities/static/js/activities/directives.js
+++ activities/static/js/activities/directives.js
@@ -124,64 +124,75 @@
         };
     });
 
-    app.directive('slidesreveal', function () {
+    app.directive('slidesreveal', [
+                  'Progress',
+                  'Answer',
+                  function (Progress, Answer) {
         return {
             restrict: 'E',
             templateUrl: '/static/templates/directives/slidesreveal.html',
             transclude: true,
-            controller: [
-              '$scope',
-              '$sce',
-              'Progress',
-              'Answer',
-              function ($scope, $sce, Progress, Answer) {
+            link:
+              function (scope, element, attrs) {
 
                 // Ensures that the slides get updated on a directive partial reload
-                $scope.reset = function() {
+                scope.reset = function() {
+
                     // Create a StudentProgress instance without 'complete' information
                     // This process is similar on other lessons and executed by lesson/controller.js, but must be remade here, since slidesreveal use its own controller
-                    Progress.save({unit: $scope.currentUnit.id});
+                    Progress.save({unit: scope.currentUnit.id});
 
-                    $scope.iframe_url = "/activity/slides_reveal/"+$scope.currentActivity.id;
+                    scope.iframe_url = "/activity/slides_reveal/"+scope.currentActivity.id;
 
                     // Try to get an Answer object for the current activity
                     // If there is none, populate a new answer object to be saved later
-                    Answer.get({activityId: $scope.currentActivity.id}, function(answer){
+                    Answer.get({activityId: scope.currentActivity.id}, function(answer){
                         if(answer.given !== undefined && answer.given.current_slide !== undefined){
                             // Store the slide number from previous session, but wait to restore it when the iframe is ready
-                            $scope.comeback_slide = answer.given.current_slide;
+                            scope.comeback_slide = answer.given.current_slide;
                         }
-                        $scope.answer = answer;
+                        scope.answer = answer;
                     }, function(error){
-                        $scope.answer = new Answer();
-                        $scope.answer.activity = $scope.currentActivity.id;
-                        $scope.answer.given = {'currentSlide': 0};
-                        $scope.answer.$save();
+                        scope.answer = new Answer();
+                        scope.answer.activity = scope.currentActivity.id;
+                        scope.answer.given = {'currentSlide': 0};
+                        scope.answer.$save();
                     });
 
                     // Find out how many slides there are in the current reveal.js iframe
                     $(function(){
                         $('#slidesreveal').on('load', function(){
-                            $scope.totalSlides = $("#slidesreveal").contents().find("div.slides")[0].childElementCount;
+                            try {
+                                scope.totalSlides = $("#slidesreveal").contents().find("div.slides")[0].childElementCount;
+                            } catch (e) {
+                                // Problem while trying to get the total from totalSlides access the reveal.js iframe
+                                // Pass it silently
+                            }
 
-                            // Remove native controls from the iframe
-                            $("#slidesreveal").contents().find("aside.controls")[0].remove();
+                            try {
+                                // Remove native controls from the iframe
+                                $("#slidesreveal").contents().find("aside.controls")[0].remove();
+                            } catch (e) {
+                                // Problem while trying to remove control arrows in the reveal.js iframe
+                                // Pass it silently
+                            }
 
                             // If the activity has a slide record from a previous session, restore it now
-                            if($scope.comeback_slide !== undefined)
-                                $scope.select_slide($scope.comeback_slide);
+                            if(scope.comeback_slide !== undefined)
+                                scope.select_slide(scope.comeback_slide);
                             else
-                                $scope.select_slide(0);
+                                scope.select_slide(0);
 
-                            // ensures that $scope.totalSlides is updated on the template
-                            $scope.$apply();
                         });
                     });
                 };
-                $scope.$watch('currentActivity', $scope.reset);
+                var watcher = scope.$watch('currentActivity', scope.reset);
+                element.on('$destroy', function () {
+                    watcher();
+                });
 
                 // Select a slide directly
-                $scope.select_slide = function(new_slide){
+                scope.select_slide = function(new_slide){
                     // Find our presentation iframe
                     var frame = document.querySelector( '#slidesreveal' );
 
@@ -190,46 +201,44 @@
                       method: 'slide',
                       args: [ new_slide ]
                     }), '*' );
-                    $scope.current_slide = new_slide;
-                    $scope.update_answer(new_slide);
+                    scope.current_slide = new_slide;
+                    scope.update_answer(new_slide);
                 };
 
                 // Go foward one slide
-                $scope.next_slide = function(){
-
+                scope.next_slide = function(){
                     // If the user alreay is in the last slide, update progress instead
-                    if($scope.current_slide === ($scope.totalSlides-1)){
-                        $scope.update_progress();
+                    if(scope.current_slide === (scope.totalSlides-1)){
+                        scope.update_progress();
                         // Open next unit
-                        $scope.nextUnit();
+                        scope.nextUnit();
 
                     } else {
-                      $scope.select_slide($scope.current_slide+1);
+                      scope.select_slide(scope.current_slide+1);
                     }
 
                 };
 
                 // Go back one slide
-                $scope.previous_slide = function(){
-                  $scope.select_slide($scope.current_slide-1);
+                scope.previous_slide = function(){
+                  scope.select_slide(scope.current_slide-1);
                 };
 
                 // The Answer instance for this activity must always store the last slide viewed
                 // Therefore, that instance must always be updated in every slide turn
-                $scope.update_answer = function(slide_number) {
+                scope.update_answer = function(slide_number) {
                     // Update the answer object
-                    $scope.answer.given = {'current_slide': slide_number};
-                    $scope.answer.$update({activityId: $scope.currentActivity.id});
+                    scope.answer.given = {'current_slide': slide_number};
+                    scope.answer.$update({activityId: scope.currentActivity.id});
                 };
 
                 // If this is the last slide, update StudentProgress with the is_complete flag
-                $scope.update_progress = function() {
-                    Progress.complete($scope.currentUnit.id);
+                scope.update_progress = function() {
+                    Progress.complete(scope.currentUnit.id);
                 };
 
               }
-            ],
         };
-    });
+    }]);
 
 })(angular);
