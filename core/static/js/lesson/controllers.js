997c18e31e1dc39ea4c5105a71eafdc4d783eac9
Bruno Martin <bruno.martin@hacklab.com.br>
2017-02-25 20:11:53 -0300

    remove unecessary code

--- core/static/js/lesson/controllers.js
+++ core/static/js/lesson/controllers.js
@@ -145,13 +145,6 @@
                 $scope.nextUnit();
             };
 
-            $scope.sendAnswerText = function() {
-                var progress;
-                progress = Progress.complete($scope.currentUnit.id);
-                $scope.currentUnit.progress = progress;
-                $scope.nextUnit();
-            };
-
             $scope.nextStep = function(skipComment) {
                 var progress;
                 if($scope.section === 'video') {
