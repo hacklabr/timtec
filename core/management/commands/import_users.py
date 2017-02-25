56b5b9fc61b2afd511981fcc645c8c9edd17d784
Bruno Martin <bruno.martin@hacklab.com.br>
2017-02-25 20:09:27 -0300

    improved import users from csv file example

--- core/management/commands/import_users.py
+++ core/management/commands/import_users.py
@@ -4,14 +4,17 @@ from django.contrib.auth import get_user_model, models
 from django.db import transaction
 
 import unicodecsv
+import random
 
 User = get_user_model()
 
-sizes = {
-    'username': 30,
-    'last_name': 30,
-    'first_name': 30,
-}
+collumns = [
+    'RF',
+    'name',
+    'job',
+    'cell',
+    'email',
+]
 
 
 class Command(BaseCommand):
@@ -24,20 +27,45 @@ class Command(BaseCommand):
         if not len(files) == 1:
             raise CommandError('No file to import')
 
+        group, _ = models.Group.objects.get_or_create(name="Associados")
+
         with open(files[0], 'r') as csvfile:
             readf = unicodecsv.DictReader(csvfile)
             count = 0
+
+            emails = []
+            usernames = []
             for row in readf:
-                for fieldname, size in sizes.items():
-                    if fieldname in row:
-                        row[fieldname] = row[fieldname][:size]
-                set_password = row.pop('set_password')
-                nu = User.objects.create(**row)
-                nu.set_password(set_password)
-                nu.is_if_staff = True
-                nu.save()
-                if nu.cpf:  # only valid for IfUsers, remove if you don't need it
-                    nu.groups.add(models.Group.objects.get(name="professors"))
+                if row.has_key('email') and row.get('email'):
+                    email = row.get('email')
+                    if email in emails:
+                        self.stdout.write(u'Email ja existe: ' + email)
+                    else:
+                        emails.append(email)
+
+                    cpf = row.get('cpf')
+                    if User.objects.filter(username=cpf).exists():
+                        self.stdout.write(u'Usuário ja existe: ' + cpf)
+                    else:
+                        usernames.append(cpf)
+
+                user = User()
+                user.first_name, user.last_name = row.get('name').split(' ', 1 )
+                if row.has_key('email') and row.get('email'):
+                    user.email = row.get('email')
+                else:
+                    user.email = str(random.randint(1,100000000)) + '@nomail.com'
+
+                if row.has_key('occupation'):
+                    user.occupation = row.get('occupation')
+                cpf = row.get('cpf')
+                user.username = user.cpf = cpf
+                user.rg = row.get('RF')
+                user.is_active = False
+                user.save()
+                user.groups.add(group)
+                user.save()
+
                 count += 1
-                if count % 10 == 0:
-                    print '.',
+                if count % 100 == 0:
+                    self.stdout.write(str(count))
