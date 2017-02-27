364385585517e977f04c03b0cd6bd3dce87e6696
Laury Bueno <laury.bueno@gmail.com>
2017-02-27 15:18:44 -0300

    Live sessions now use public channels

--- core/static/js/lesson/directives.js
+++ core/static/js/lesson/directives.js
@@ -12,7 +12,7 @@
             },
             'controller': ['$scope', '$sce', function($scope, $sce) {
                 $scope.getChatUrl = function (chat_room) {
-                    return $sce.trustAsResourceUrl(window.chat_address+ '/group/' +chat_room);
+                    return $sce.trustAsResourceUrl(window.chat_address+ '/channel/' +chat_room);
                 };
             }]
         };
