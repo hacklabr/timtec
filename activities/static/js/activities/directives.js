203256588d5ba206eba2f596bb90053e9fd73481
Laury Bueno <laury@hacklab.com.br>
2017-08-30 18:09:36 -0300

    [SlidesRevealActivity] Fragments are now supported

--- activities/static/js/activities/directives.js
+++ activities/static/js/activities/directives.js
@@ -134,6 +134,8 @@
             transclude: true,
             link:
               function (scope, element, attrs) {
+                
+                var frame;  // holds a pointer to the #slidesreveal iframe
 
                 // Ensures that the slides get updated on a directive partial reload
                 var reset = function() {
@@ -148,28 +150,15 @@
                     // This process is similar on other lessons and executed by lesson/controller.js, but must be remade here, since slidesreveal use its own controller
                     Progress.save({unit: scope.currentUnit.id});
 
-                    scope.iframe_url = "/activity/slides_reveal/"+scope.currentActivity.id;
-
-                    // Try to get an Answer object for the current activity
-                    // If there is none, populate a new answer object to be saved later
-                    Answer.get({activityId: scope.currentActivity.id}, function(answer){
-                        if(answer.given !== undefined && answer.given.current_slide !== undefined){
-                            // Store the slide number from previous session, but wait to restore it when the iframe is ready
-                            scope.comeback_slide = answer.given.current_slide;
-                        }
-                        scope.answer = answer;
-                    }, function(error){
-                        scope.answer = new Answer();
-                        scope.answer.activity = scope.currentActivity.id;
-                        scope.answer.given = {'currentSlide': 0};
-                        scope.answer.$save();
-                    });
+                    scope.iframe_url = "/activity/slides_reveal/" + scope.currentActivity.id;
 
                     // Find out how many slides there are in the current reveal.js iframe
+                    // But only do so if its done loading
                     $(function(){
                         $('#slidesreveal').on('load', function(){
                             try {
                                 scope.totalSlides = $("#slidesreveal").contents().find("div.slides")[0].childElementCount;
+                                frame = document.querySelector( '#slidesreveal' );
                             } catch (e) {
                                 // Problem while trying to get the total from the reveal.js iframe
                                 // Pass it silently
@@ -183,12 +172,7 @@
                                 // Pass it silently
                             }
 
-                            // If the activity has a slide record from a previous session, restore it now
-                            if(scope.comeback_slide !== undefined)
-                                scope.select_slide(scope.comeback_slide);
-                            else
-                                scope.select_slide(0);
-
+                            scope.$apply();
                         });
                     });
                 };
@@ -199,53 +183,71 @@
 
                 // Select a slide directly
                 scope.select_slide = function(new_slide){
-                    // Find our presentation iframe
-                    var frame = document.querySelector( '#slidesreveal' );
-
                     // Command the iframe to open a specific slide via message API
                     frame.contentWindow.postMessage( JSON.stringify({
-                      method: 'slide',
-                      args: [ new_slide ]
+                        method: 'slide',
+                        args: [ new_slide ]
                     }), '*' );
-                    scope.current_slide = new_slide;
-                    scope.update_answer(new_slide);
                 };
+
+                // The following var marks if the reveal slider responded the last call from 'next_slide'
+                // If it has not, there are no more slides to show and the 'nextUnit' must be called
+                var waiting_reveal_feedback = false;
 
                 // Go foward one slide
                 scope.next_slide = function(){
-                    // If the user alreay is in the last slide, update progress instead
-                    if(scope.current_slide === (scope.totalSlides-1)){
-                        scope.update_progress();
-                        // Show the next interaction for the current slides module
+                    if(waiting_reveal_feedback){
                         scope.nextStep();
-
-                    } else {
-                      scope.select_slide(scope.current_slide+1);
-                    }
-
+                        waiting_reveal_feedback = false;
+                    } else
+                        waiting_reveal_feedback = true;
+                    frame.contentWindow.postMessage( JSON.stringify({
+                        method: 'right',
+                        args: [  ]
+                    }), '*' );
                 };
 
                 // Go back one slide
                 scope.previous_slide = function(){
-                  // If the user alreay is in the first slide, don't change the number
-                  if(scope.current_slide > 0)
-                      scope.select_slide(scope.current_slide-1);
+                    frame.contentWindow.postMessage( JSON.stringify({
+                    method: 'left',
+                    args: [  ]
+                    }), '*' );
                 };
 
-                // The Answer instance for this activity must always store the last slide viewed
-                // Therefore, that instance must always be updated in every slide turn
-                scope.update_answer = function(slide_number) {
-                    // Update the answer object
-                    scope.answer.given = {'current_slide': slide_number};
-                    scope.answer.$update({activityId: scope.currentActivity.id});
-                };
+                window.addEventListener( 'message', function( event ) {
+                    var data = JSON.parse( event.data );
+                    // Make sure we're talking to a presentation
+                    if( data.namespace === 'reveal' ) {
+                        if( data.eventName === 'slidechanged' ||
+                            data.eventName === 'ready' ||
+                            data.eventName === 'fragmentshown') {
+
+                            waiting_reveal_feedback = false;
+
+                            // Dig out the presentation state, key properties:
+                            //   indexh: The index of the current horizontal slide
+                            //   indexv: The index of the current vertical slide (if any)
+                            //   indexf: The index of the current fragment (if any)
+                            var state = data.state;
+                            scope.current_slide = state.indexh;
+                            scope.$apply();
+
+                            // Save progress if this is the last slide
+                            if (scope.current_slide === (scope.totalSlides-1))
+                                scope.update_progress();
+                        }
+                    }
+                });
 
                 // If this is the last slide, update StudentProgress with the is_complete flag
                 scope.update_progress = function() {
-                    scope.currentUnit.progress = Progress.complete(scope.currentUnit.id);
+                    Progress.complete(scope.currentUnit.id);
+                    var unit_position = scope.findUnitPos(scope.currentUnit);
+                    scope.lesson.units[unit_position].progress.complete = true;
                 };
 
-              }
+            }
         };
     }]);
 
