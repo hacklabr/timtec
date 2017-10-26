655058490c27a16d7823ba8d28e8bbf00fc18e37
Virgílio Santos <virgilio.santos@gmail.com>
2017-10-26 21:09:49 -0200

    issue#444: remove both password filled condition

--- accounts/forms.py
+++ accounts/forms.py
@@ -41,9 +41,8 @@ class ProfileEditForm(BaseProfileEditForm):
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
-        if password1 and password2:
-            if password1 != password2:
-                raise forms.ValidationError(_("The two password fields didn't match."))
+        if password1 != password2:
+            raise forms.ValidationError(_("The two password fields didn't match."))
         return password2
 
     def save(self, commit=True):
