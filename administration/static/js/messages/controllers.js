7158f6031bfd09c4ab17218c03aff30df687a596
Leonardo Piccioni de Almeida <leopiccionia@gmail.com>
2017-11-22 19:24:50 -0200

    [GlobalMessages] Showing combo box instead of radio.

--- administration/static/js/messages/controllers.js
+++ administration/static/js/messages/controllers.js
@@ -11,190 +11,194 @@
     });
 
     module.controller('NewMessageController', ['$scope', '$uibModal', '$window', 'Message', 'Student', 'StudentSearch', 'Class', 'messages_list', '$rootScope', 'ContentFile', 'uiTinymceConfig',
-            function($scope, $uibModal, $window, Message, Student, StudentSearch, Class, messages_list, $rootScope, ContentFile, uiTinymceConfig) {
-                $scope.course_id = parseInt($window.course_id, 10);
-                $scope.messages = messages_list.messages;
+        function($scope, $uibModal, $window, Message, Student, StudentSearch, Class, messages_list, $rootScope, ContentFile, uiTinymceConfig) {
+            $scope.course_id = parseInt($window.course_id, 10);
+            $scope.messages = messages_list.messages;
 
-                uiTinymceConfig.images_upload_handler = ContentFile.upload;
+            uiTinymceConfig.images_upload_handler = ContentFile.upload;
 
-                $scope.new_message = function () {
-                    var modalInstance = $uibModal.open({
-                        templateUrl: 'newMessageModal.html',
-                        controller: ['$scope', '$uibModalInstance', 'course_id', SendMessageModalInstanceCtrl],
-                        resolve: {
-                            course_id: function () {
-                                return $scope.course_id;
-                            }
+            $scope.new_message = function () {
+                var modalInstance = $uibModal.open({
+                    templateUrl: 'newMessageModal.html',
+                    controller: ['$scope', '$uibModalInstance', 'course_id', SendMessageModalInstanceCtrl],
+                    resolve: {
+                        course_id: function () {
+                            return $scope.course_id;
                         }
-                    });
-                    modalInstance.result.then(function (new_message) {
-                        new_message.$save({}, function(new_message){
-                            messages_list.messages.unshift(new_message);
-                            $rootScope.$broadcast('newMessage');
-                        });
-
-                    });
-                };
-                var SendMessageModalInstanceCtrl = function ($scope, $uibModalInstance, course_id) {
-
-                    $scope.new_message = new Message();
-                    $scope.new_message.course = course_id;
-                    $scope.new_message.users = [];
-                    $scope.recipient_list = [];
-                    $scope.empty_msg_subject_error = false;
-                    $scope.empty_msg_body_error = false;
-
-                    $scope.classes = Class.query({course: course_id}, function(classes){
-                        classes.checked = [];
-                        $scope.classes_ready = true;
-                        return classes;
+                    }
+                });
+                modalInstance.result.then(function (new_message) {
+                    new_message.$save({}, function(new_message){
+                        messages_list.messages.unshift(new_message);
+                        $rootScope.$broadcast('newMessage');
                     });
 
-                    // trick to user modal.all_checked in ng-model html tag
-                    $scope.modal = {};
-                    $scope.modal.all_checked = true;
+                });
+            };
+            var SendMessageModalInstanceCtrl = function ($scope, $uibModalInstance, course_id) {
 
-                    $scope.send = function () {
-                        // TODO validação dos campo: títle e message não podem ser vazios
-                        if ($scope.modal.all_checked) {
-                            $scope.new_message.users = [];
-                            angular.forEach($scope.classes, function(klass) {
-                                $scope.new_message.users = $scope.new_message.users.concat(klass.students);
-                            });
-                        } else if ($scope.classes.checked) {
-                            angular.forEach($scope.classes.checked, function(klass) {
-                                $scope.new_message.users = $scope.new_message.users.concat(klass);
-                            });
-                        }
-                        if ($scope.new_message.message && $scope.new_message.subject) {
-                            $uibModalInstance.close($scope.new_message);
-                            $scope.empty_msg_subject_error = false;
-                            $scope.empty_msg_body_error = false;
-                        }
-                        if (!$scope.new_message.message) {
-                            $scope.empty_msg_body_error = true;
-                        } else {
-                            $scope.empty_msg_body_error = false;
-                        }
-                        if (!$scope.new_message.subject) {
-                            $scope.empty_msg_subject_error = true;
-                        } else {
-                            $scope.empty_msg_subject_error = false;
-                        }
-                    };
+                $scope.new_message = new Message();
+                $scope.new_message.course = course_id;
+                $scope.new_message.users = [];
+                $scope.recipient_list = [];
+                $scope.empty_msg_subject_error = false;
+                $scope.empty_msg_body_error = false;
 
-                    $scope.cancel = function () {
-                        $uibModalInstance.dismiss();
-                    };
-                    $scope.getUsers = function(val) {
-                        return new StudentSearch(val, course_id);
-                    };
+                $scope.classes = Class.query({course: course_id}, function(classes){
+                    classes.checked = [];
+                    $scope.classes_ready = true;
+                    return classes;
+                });
 
-                    $scope.on_select_student = function(model) {
-                        $scope.new_message.users.unshift(model.id);
-                        $scope.recipient_list.unshift(model);
-                        $scope.asyncSelected = '';
-                    };
+                // trick to user modal.all_checked in ng-model html tag
+                $scope.modal = {};
+                $scope.modal.all_checked = true;
 
-                    $scope.remove_student = function(index) {
-                        $scope.new_message.users.splice(index, 1);
-                        $scope.recipient_list.splice(index, 1);
-                    };
-
-                };
-            }
-        ]);
-
-        module.controller('NewGlobalMessageController', ['$scope', '$uibModal', '$window', 'MessageGlobal', 'Student', 'Group', 'UserSearch', 'Class', 'messages_list', '$rootScope', 'ContentFile', 'uiTinymceConfig',
-                function($scope, $uibModal, $window, MessageGlobal, Student, Group, UserSearch, Class, messages_list, $rootScope, ContentFile, uiTinymceConfig) {
-                    $scope.course_id = parseInt($window.course_id, 10);
-                    $scope.messages = messages_list.messages;
-
-                    uiTinymceConfig.images_upload_handler = ContentFile.upload;
-
-                    $scope.new_message = function () {
-                        var modalInstance = $uibModal.open({
-                            templateUrl: 'newMessageModal.html',
-                            controller: ['$scope', '$uibModalInstance', 'course_id', SendMessageModalInstanceCtrl],
-                            resolve: {
-                                course_id: function () {
-                                    return $scope.course_id;
-                                }
-                            }
-                        });
-                        modalInstance.result.then(function (new_message) {
-                            new_message.$save({}, function(new_message){
-                                messages_list.messages.unshift(new_message);
-                                $rootScope.$broadcast('newMessage');
-                            });
-
-                        });
-                    };
-                    var SendMessageModalInstanceCtrl = function ($scope, $uibModalInstance) {
-
-                        // Get groups listing
-                        Group.query({}, function (groups) {
-                            $scope.groups = groups;
-                        });
-
-                        $scope.new_message = new MessageGlobal();
+                $scope.send = function () {
+                    // TODO validação dos campo: títle e message não podem ser vazios
+                    if ($scope.modal.all_checked) {
                         $scope.new_message.users = [];
-                        $scope.recipient_list = [];
+                        angular.forEach($scope.classes, function(klass) {
+                            $scope.new_message.users = $scope.new_message.users.concat(klass.students);
+                        });
+                    } else if ($scope.classes.checked) {
+                        angular.forEach($scope.classes.checked, function(klass) {
+                            $scope.new_message.users = $scope.new_message.users.concat(klass);
+                        });
+                    }
+                    if ($scope.new_message.message && $scope.new_message.subject) {
+                        $uibModalInstance.close($scope.new_message);
                         $scope.empty_msg_subject_error = false;
                         $scope.empty_msg_body_error = false;
+                    }
+                    if (!$scope.new_message.message) {
+                        $scope.empty_msg_body_error = true;
+                    } else {
+                        $scope.empty_msg_body_error = false;
+                    }
+                    if (!$scope.new_message.subject) {
+                        $scope.empty_msg_subject_error = true;
+                    } else {
+                        $scope.empty_msg_subject_error = false;
+                    }
+                };
 
-                        // trick to user modal.all_checked in ng-model html tag
-                        $scope.modal = {};
-                        $scope.modal.all_students = true;
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss();
+                };
+                $scope.getUsers = function(val) {
+                    return new StudentSearch(val, course_id);
+                };
 
-                        $scope.send = function () {
-                            if ($scope.modal.all_students){
-                                $scope.new_message.all_students = true;
-                                delete $scope.new_message.users;  // ensures that the empty array wont be sent to the server
-                            }
-                            // If groups were specified, put them in a groups field
-                            if ($scope.groups.checked){
-                                $scope.new_message.groups = $scope.groups.checked;
-                                delete $scope.new_message.users;  // ensures that the empty array wont be sent to the server
-                            }
-                            if ($scope.new_message.message && $scope.new_message.subject) {
-                                $uibModalInstance.close($scope.new_message);
-                                $scope.empty_msg_subject_error = false;
-                                $scope.empty_msg_body_error = false;
-                            }
-                            if (!$scope.new_message.message) {
-                                $scope.empty_msg_body_error = true;
-                            } else {
-                                $scope.empty_msg_body_error = false;
-                            }
-                            if (!$scope.new_message.subject) {
-                                $scope.empty_msg_subject_error = true;
-                            } else {
-                                $scope.empty_msg_subject_error = false;
-                            }
-                        };
+                $scope.on_select_student = function(model) {
+                    $scope.new_message.users.unshift(model.id);
+                    $scope.recipient_list.unshift(model);
+                    $scope.asyncSelected = '';
+                };
 
-                        $scope.cancel = function () {
-                            $uibModalInstance.dismiss();
-                        };
-                        $scope.getUsers = function(val) {
-                            return new UserSearch(val);
-                        };
+                $scope.remove_student = function(index) {
+                    $scope.new_message.users.splice(index, 1);
+                    $scope.recipient_list.splice(index, 1);
+                };
 
-                        $scope.on_select_student = function(model) {
-                            $scope.new_message.users.unshift(model.id);
-                            $scope.recipient_list.unshift(model);
-                            $scope.asyncSelected = '';
-                        };
+            };
+        }
+    ]);
 
-                        $scope.remove_student = function(index) {
-                            $scope.new_message.users.splice(index, 1);
-                            $scope.recipient_list.splice(index, 1);
-                        };
+    module.controller('NewGlobalMessageController', ['$scope', '$uibModal', '$window', 'MessageGlobal', 'Student', 'Group', 'UserSearch', 'Class', 'messages_list', '$rootScope', '$sce', 'ContentFile', 'uiTinymceConfig',
+        function($scope, $uibModal, $window, MessageGlobal, Student, Group, UserSearch, Class, messages_list, $rootScope, $sce, ContentFile, uiTinymceConfig) {
+            $scope.course_id = parseInt($window.course_id, 10);
+            $scope.messages = messages_list.messages;
 
-                    };
-                }
-            ]);
+            uiTinymceConfig.images_upload_handler = ContentFile.upload;
+
+            $scope.new_message = function () {
+                var modalInstance = $uibModal.open({
+                    templateUrl: 'newMessageModal.html',
+                    controller: ['$scope', '$uibModalInstance', 'course_id', SendMessageModalInstanceCtrl],
+                    resolve: {
+                        course_id: function () {
+                            return $scope.course_id;
+                        }
+                    }
+                });
+                modalInstance.result.then(function (new_message) {
+                    new_message.$save({}, function(new_message){
+                        messages_list.messages.unshift(new_message);
+                        $rootScope.$broadcast('newMessage');
+                    });
+
+                });
+            };
+            var SendMessageModalInstanceCtrl = function ($scope, $uibModalInstance) {
+
+                // Get groups listing
+                Group.query({}, function (groups) {
+                    $scope.groups = groups;
+                });
+
+                $scope.get_as_safe_html = function(value) {
+                    return $sce.trustAsHtml(value);
+                };
+
+                $scope.new_message = new MessageGlobal();
+                $scope.new_message.users = [];
+                $scope.recipient_list = [];
+                $scope.empty_msg_subject_error = false;
+                $scope.empty_msg_body_error = false;
+
+                // trick to user modal.all_checked in ng-model html tag
+                $scope.modal = {};
+                $scope.modal.all_students = true;
+
+                $scope.send = function () {
+                    if ($scope.modal.all_students){
+                        $scope.new_message.all_students = true;
+                        delete $scope.new_message.users;  // ensures that the empty array wont be sent to the server
+                    }
+                    // If groups were specified, put them in a groups field
+                    if ($scope.groups.checked){
+                        $scope.new_message.groups = $scope.groups.checked;
+                        delete $scope.new_message.users;  // ensures that the empty array wont be sent to the server
+                    }
+                    if ($scope.new_message.message && $scope.new_message.subject) {
+                        $uibModalInstance.close($scope.new_message);
+                        $scope.empty_msg_subject_error = false;
+                        $scope.empty_msg_body_error = false;
+                    }
+                    if (!$scope.new_message.message) {
+                        $scope.empty_msg_body_error = true;
+                    } else {
+                        $scope.empty_msg_body_error = false;
+                    }
+                    if (!$scope.new_message.subject) {
+                        $scope.empty_msg_subject_error = true;
+                    } else {
+                        $scope.empty_msg_subject_error = false;
+                    }
+                };
+
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss();
+                };
+                $scope.getUsers = function(val) {
+                    return new UserSearch(val);
+                };
+
+                $scope.on_select_student = function(model) {
+                    $scope.new_message.users.unshift(model.id);
+                    $scope.recipient_list.unshift(model);
+                    $scope.asyncSelected = '';
+                };
+
+                $scope.remove_student = function(index) {
+                    $scope.new_message.users.splice(index, 1);
+                    $scope.recipient_list.splice(index, 1);
+                };
+
+            };
+        }
+    ]);
 
     module.controller('MessagesListController', ['$scope', '$uibModal', '$sce', '$window', 'Message', 'messages_list',
         function($scope, $uibModal, $sce, $window, Message, messages_list) {
